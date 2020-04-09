b9926c666dc0fef77edfeaf5615c956ccde9bfd3
Abhishek Sharma <abhisharma.sharma7@gmail.com>
2020-04-09 11:51:10 +0530

    class fields

--- app/components/Results.js
+++ app/components/Results.js
@@ -48,18 +48,13 @@ ProfileList.propTypes = {
 }
 
 export default class Results extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      winner: null,
-      loser: null,
-      error: null,
-      loading: true
-    }
+  state = {
+    winner: null,
+    loser: null,
+    error: null,
+    loading: true
   }
   componentDidMount () {
-    // debugger
     const { playerOne, playerTwo } = queryString.parse(this.props.location.search)
 
     battle([ playerOne, playerTwo ])
